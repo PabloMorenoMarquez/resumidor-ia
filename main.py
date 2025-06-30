1b677ec87d449c4d53acfcf81fe3f3645d9950fa
José luis Moreno <pablomorenomarquez@gmail.com>
2025-06-30 18:06:07 +0200

    Añadido OpenAI Key

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import openai, fitz, markdown2, requests
 import os
 from fpdf import FPDF
 
-openai.api_key = "Tu_clave_API"
+openai.api_key = os.environ.get("OPENAI_API_KEY")
 
 app = Flask(__name__)
 
